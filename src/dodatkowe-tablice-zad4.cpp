2e18b2489438d674dee2f5b573da2c544dfa326c
MKawcz <s25441@pjwstk.edu.pl>
2021-11-22 18:06:36 +0100

    kolkoikrzyzyk

--- src/dodatkowe-tablice-zad4.cpp
+++ src/dodatkowe-tablice-zad4.cpp
@@ -12,15 +12,13 @@ auto ask_user_for_integer(std::string prompt) -> int
 auto main() -> int
 {
     auto const a = ask_user_for_integer("Podaj wysokosc: ");
-    int b;
-    b = a;
-    int tab[a][b];
+    int tab[1000][1000];
 
     tab[0][0] = 1;
     tab[1][0] = 1;
     tab[1][1] = 1;
 
-    for (auto i = 2; i <= a; i++) {
+    for (auto i = 2; i < a; i++) {
         for (auto j = 0; j <= i; j++) {
             if (i == j) {
                 tab[j][i] = 1;
@@ -31,8 +29,15 @@ auto main() -> int
             }
 
             tab[i][j] = tab[i - 1][j - 1] + tab[i - 1][j];
-            std::cout << tab[i][j];
         }
+    }
+
+
+    for (auto k = 0; k < a; k++) {
+        for (auto l = 0; l <= k; l++) {
+            std::cout << tab[k][l] << " ";
+        }
+        std::cout << std::endl;
     }
 
 
